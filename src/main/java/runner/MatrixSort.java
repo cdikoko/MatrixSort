783ae92c346b33b68345694a7d212b6351a07700
cdikoko <cdikoko@stu.jsu.edu>
2017-07-24 08:17:30 -0500

    Restart

--- src/main/java/runner/MatrixSort.java
+++ src/main/java/runner/MatrixSort.java
@@ -27,68 +27,28 @@ public class MatrixSort {
 	public static String col;
 
 	public MatrixSort(int value) {
-		String path = "src/main/resources/Clement Task.xlsx";
+		String path = "src/main/resources/Clement Task .xlsx";
 		new MatrixSort(path);
-		convert(find(list, value));
+		find(list, value);
+		convert(getIndex());
 
 	}
 
-	private static String file = "src/main/resources/Clement Task.xlsx";
+	
+
+	private static String getIndex() {
+		// gets a string version of the index: Location in Array 
+		return String.valueOf(index);
+	}
+
+
 
 	public static void main(String[] args) {
-		new MatrixSort(5005);
+		new MatrixSort(79);
 
 		//numbers u wanted were: 55, 35, 11, 2222 and 200,000
 	}
 
-	public static String find(ArrayList<Integer> s, int i) {
-		number = String.valueOf(i);
-
-		for (int myValue : s)
-			if (myValue == i) {
-
-				index = s.indexOf(i);
-				setLocation(String.valueOf(index));
-
-				if (number.length() < 2)
-					setLocation("0" + getLocation());
-
-			}
-
-		return getLocation();
-
-	}
-
-	public static String convert(String rowColIndex) {
-
-		String convert = "";
-		String result = "";
-
-		//each row needs to added by 1 to get the actual row;
-		int actualRow = 0;
-
-		if (rowColIndex.length() >= 2) {
-			row = rowColIndex.substring(0, 1);
-			col = rowColIndex.substring(1);
-			convert = CellReference.convertNumToColString(Integer.valueOf(col));
-			actualRow = Integer.valueOf(row) + 1;
-			setLocation(convert + actualRow);
-			System.out.print("\n The number " + number + " is located at cell: " + getLocation());
-
-		}
-
-		if (rowColIndex.length() < 2) {
-			row = "1";
-			col = rowColIndex;
-			convert = CellReference.convertNumToColString(Integer.valueOf(col));
-			setLocation(convert + row);
-			result = "\n The number " + number + " is located at cell: " + getLocation();
-
-		}
-
-		return result;
-
-	}
 
 	public MatrixSort(String path) {
 		try {
@@ -107,8 +67,7 @@ public class MatrixSort {
 
 					switch (cell.getCellType()) {
 					case Cell.CELL_TYPE_STRING:
-
-						//System.out.print("\n" + cell.getStringCellValue() + "\t");
+						System.out.print("Please Use Numerical Values \t");
 						break;
 					case Cell.CELL_TYPE_NUMERIC:
 
@@ -116,7 +75,7 @@ public class MatrixSort {
 						//System.out.print(cell.getNumericCellValue() + "\t");
 						break;
 					case Cell.CELL_TYPE_BOOLEAN:
-						//System.out.print(cell.getBooleanCellValue() + "\t");
+						System.out.print("Please Use Numerical Values");
 						break;
 					default:
 
@@ -134,17 +93,5 @@ public class MatrixSort {
 
 	}
 
-	/**
-	 * @return the location
-	 */
-	public static String getLocation() {
-		return location;
-	}
 
-	/**
-	 * @param location the location to set
-	 */
-	public static void setLocation(String location) {
-		MatrixSort.location = location;
-	}
 }
